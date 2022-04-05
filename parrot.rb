bed5d8a59283c046bdd17d0ee1c1b476e72d712d
wesley <wesbourkeavl@gmail.com>
2022-04-05 10:19:38 -0400

    commit tree 8d7cbf1f99576d29cde394c2f9ca1178c4b4c96e

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+# require 'pry'
+
+def parrot(words = "Squawk!")
+    puts words
+    # binding.pry
+    return words
+end    
+
+parrot()
